e2566c97eb7c872834bf9251379de0ad1fe14848
Allen Porter <allen.porter@gmail.com>
2020-08-23 22:51:13 -0700

    Add a singleton for storing crime records
    Currently stores dummy values

---
+++ app/src/main/java/android/bignerdranch/com/CrimeLab.java
@@ -0,0 +1,44 @@
+package android.bignerdranch.com;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.UUID;
+
+/**
+ * Singleton for holding crime records.
+ *
+ * Note, this class is not threadsafe.
+ */
+final class CrimeLab {
+  private static CrimeLab sCrimeLab;
+  private final Map<UUID, Crime> mCrimes = new TreeMap<UUID, Crime>();
+
+  public static CrimeLab get() {
+    if (sCrimeLab == null) {
+      sCrimeLab = new CrimeLab();
+    }
+    return sCrimeLab;
+  }
+
+  private CrimeLab() {
+    for (int i = 0; i < 100; ++i) {
+      Crime crime = new Crime();
+      crime.setTitle("Crime #" + i);
+      crime.setSolved(i % 2 == 0);
+      mCrimes.put(crime.getId(), crime);
+    }
+  }
+
+  public List<Crime> getCrimes() {
+    return new ArrayList<Crime>(mCrimes.values());
+  }
+
+  @Nullable
+  public Crime getCrime(UUID id) {
+    return mCrimes.get(id);
+  }
+}
