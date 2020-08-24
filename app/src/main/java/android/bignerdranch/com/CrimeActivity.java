4e4adda7886637636553a88f60e3cf6282af7d11
Allen Porter <allen.porter@gmail.com>
2020-08-23 22:52:13 -0700

    Add fragment to the activity
    Displays a crime info panel

--- app/src/main/java/android/bignerdranch/com/CrimeActivity.java
+++ app/src/main/java/android/bignerdranch/com/CrimeActivity.java
@@ -1,6 +1,8 @@
 package android.bignerdranch.com;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -9,6 +11,15 @@ public class CrimeActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_crime);
+    setContentView(R.layout.activity_fragment);
+
+    FragmentManager fm = getSupportFragmentManager();
+    Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+    if (fragment == null) {
+      fragment = new CrimeFragment();
+      fm.beginTransaction()
+        .add(R.id.fragment_container, fragment)
+        .commit();
+    }
   }
 }
