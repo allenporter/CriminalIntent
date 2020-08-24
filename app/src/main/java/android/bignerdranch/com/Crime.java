cbb33e854b7f29f96d5718f4ab03f9a154dfdd4c
Allen Porter <allen@thebends.org>
2020-08-23 22:06:00 -0700

    Adding getters/setters for Crime. Partially built CrimeFragment.

--- app/src/main/java/android/bignerdranch/com/Crime.java
+++ app/src/main/java/android/bignerdranch/com/Crime.java
@@ -9,8 +9,36 @@ final class Crime {
   private Date mDate;
   private boolean mSolved;
 
-  Crime() {
+  public Crime() {
     mId = UUID.randomUUID();
     mDate = new Date();
+  }
+
+  public UUID getId() {
+    return mId;
+  }
+
+  public String getTitle() {
+    return mTitle;
+  }
+
+  public void setTitle(String mTitle) {
+    this.mTitle = mTitle;
+  }
+
+  public Date getDate() {
+    return mDate;
+  }
+
+  public void setDate(Date mDate) {
+    this.mDate = mDate;
+  }
+
+  public boolean isSolved() {
+    return mSolved;
+  }
+
+  public void setSolved(boolean mSolved) {
+    this.mSolved = mSolved;
   }
 }
