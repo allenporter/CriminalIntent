428b5105e00e349bcdd672c78405d5120a70e163
Allen Porter <allen@thebends.org>
2020-09-13 12:33:12 -0700

    Handle contact picker return values, saving contacts to the DB.

--- app/src/main/java/android/bignerdranch/com/CrimeFragment.java
+++ app/src/main/java/android/bignerdranch/com/CrimeFragment.java
@@ -2,7 +2,11 @@ package android.bignerdranch.com;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -29,12 +33,14 @@ public final class CrimeFragment extends Fragment {
   private static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
 
   private static final int REQUEST_DATE = 0;
+  private static final int REQUEST_CONTACT = 1;
 
   private Crime mCrime;
   private EditText mTitleField;
   private Button mDateButton;
   private CheckBox mSovledCheckBox;
   private Button mDeleteButton;
+  private Button mSuspectButton;
   private Button mReportButton;
 
   public static CrimeFragment newInstance(UUID crimeId) {
@@ -109,6 +115,29 @@ public final class CrimeFragment extends Fragment {
       }
     });
 
+    final Intent pickContact = new Intent(Intent.ACTION_PICK,
+      ContactsContract.Contacts.CONTENT_URI);
+    mSuspectButton = (Button) v.findViewById(R.id.crime_suspect);
+    mSuspectButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        startActivityForResult(pickContact, REQUEST_CONTACT);
+      }
+    });
+    if (mCrime.getSuspect() != null) {
+      mSuspectButton.setText(mCrime.getSuspect());
+    }
+    // TODO(allen): This is meant to hide the contact picker when unsupported by the device,
+    //  however it also hides it in the simulator which does have a contact app.  Figure out
+    //  how to add this back in, fix the simulator, or do without since most devices should have
+    //  a contact picker.
+    /*
+    PackageManager packageManager = getActivity().getPackageManager();
+    if (packageManager.resolveActivity(pickContact, PackageManager.MATCH_DEFAULT_ONLY) == null) {
+      mSuspectButton.setEnabled(false);
+    }
+    */
+
     mReportButton = (Button) v.findViewById(R.id.crime_report);
     mReportButton.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -127,13 +156,31 @@ public final class CrimeFragment extends Fragment {
 
   @Override
   public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-    if (resultCode != Activity.RESULT_OK) {
+    if (resultCode != Activity.RESULT_OK || data == null) {
       return;
     }
     if (requestCode == REQUEST_DATE) {
       Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
       mCrime.setDate(date);
       updateDate();
+    }
+    if (requestCode == REQUEST_CONTACT) {
+      Uri contactUri = data.getData();
+      String[] queryFields = new String[] {
+        ContactsContract.Contacts.DISPLAY_NAME
+      };
+      Cursor c = getActivity().getContentResolver().query(contactUri, queryFields, null, null, null);
+      try {
+        if (c.getCount() == 0) {
+          return;
+        }
+        c.moveToFirst();
+        String suspect = c.getString(0);
+        mCrime.setSuspect(suspect);
+        mSuspectButton.setText(suspect);
+      } finally {
+        c.close();
+      }
     }
   }
 
