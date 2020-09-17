232ae6eed8031565c4447bded57c07b2c4de0e28
Allen Porter <allen.porter@gmail.com>
2020-09-16 23:43:36 -0700

    Add bitmap for displaying saved photo
    Handles the response from camera capture and displays the result.

--- app/src/main/java/android/bignerdranch/com/CrimeFragment.java
+++ app/src/main/java/android/bignerdranch/com/CrimeFragment.java
@@ -7,11 +7,14 @@ import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -32,10 +35,13 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.ShareCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import java.io.File;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public final class CrimeFragment extends Fragment {
@@ -44,11 +50,14 @@ public final class CrimeFragment extends Fragment {
   private static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
 
   private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+  private static final int PERMISSIONS_REQUEST_CAMERA = 2;
+
   private static String[] PERMISSIONS_CONTACT = {Manifest.permission.READ_CONTACTS};
 
   private static final int REQUEST_DATE = 0;
   private static final int REQUEST_CONTACT = 1;
   private static final int REQUEST_DIAL = 2;
+  private static final int REQUEST_PHOTO = 3;
 
   private Crime mCrime;
   private File mPhotoFile;
@@ -183,9 +192,36 @@ public final class CrimeFragment extends Fragment {
         startActivity(i);
       }
     });
+    PackageManager packageManager = getActivity().getPackageManager();
+    final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    // TODO(aporter): Ask for camera permissions.  This currently never resolves the activity right.
+    //boolean canTakePhoto = mPhotoFile != null &&
+    //  captureImage.resolveActivity(packageManager) != null;
+    //mPhotoButton.setEnabled(canTakePhoto);
 
     mPhotoButton = (ImageButton) v.findViewById(R.id.crime_camera);
+    mPhotoButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Uri uri = FileProvider.getUriForFile(getActivity(),
+          "com.bignerdranch.android.criminalintent.fileprovider",
+          mPhotoFile);
+        captureImage.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        List<ResolveInfo> cameraActivities = getActivity()
+          .getPackageManager().queryIntentActivities(captureImage,
+            PackageManager.MATCH_DEFAULT_ONLY);
+        for (ResolveInfo activity : cameraActivities) {
+          getActivity().grantUriPermission(activity.activityInfo.packageName,
+            uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        }
+        startActivityForResult(captureImage, REQUEST_PHOTO);
+      }
+    });
+
+
+
     mPhotoView = (ImageView) v.findViewById(R.id.crime_photo);
+    updatePhotoView();
 
     updateState();
 
@@ -249,6 +285,13 @@ public final class CrimeFragment extends Fragment {
       }
       updateState();
     }
+    if (requestCode == REQUEST_PHOTO) {
+      Uri uri = FileProvider.getUriForFile(getActivity(),
+      "com.bignerdranch.android.criminalintent.fileprovider",
+        mPhotoFile);
+      getActivity().revokeUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+      updatePhotoView();
+    }
   }
 
   private void requestContactPermission() {
@@ -277,6 +320,46 @@ public final class CrimeFragment extends Fragment {
             PERMISSIONS_REQUEST_READ_CONTACTS);
         }
       }
+    }
+  }
+
+
+  private void requestCameraPermission() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+          android.Manifest.permission.CAMERA)) {
+          AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+          builder.setTitle("Cameras permission");
+          builder.setPositiveButton(android.R.string.ok, null);
+          builder.setMessage("Please enable access to camera.");
+          builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @TargetApi(Build.VERSION_CODES.M)
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+              requestPermissions(
+                new String[]
+                  {Manifest.permission.CAMERA}
+                , PERMISSIONS_REQUEST_CAMERA);
+            }
+          });
+          builder.show();
+        } else {
+          ActivityCompat.requestPermissions(getActivity(),
+            new String[]{android.Manifest.permission.CAMERA},
+            PERMISSIONS_REQUEST_CAMERA);
+        }
+      }
+    }
+  }
+
+  private void updatePhotoView() {
+    if (mPhotoFile == null | !mPhotoFile.exists()) {
+      mPhotoView.setImageDrawable(null);
+    } else {
+      Bitmap bitmap = BitmapUtils.getScaledBitmap(
+        mPhotoFile.getPath(), getActivity());
+      mPhotoView.setImageBitmap(bitmap);
     }
   }
 
